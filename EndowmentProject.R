25d2c557c5851a16a473fb0409fa1fdc5d00fbbf
Chris Davidson <chrisdavidsonphd@gmail.com>
2021-08-04 20:18:05 -0400

    Update Scripts & Add Files

--- EndowmentProject.R
+++ EndowmentProject.R
@@ -1,6 +1,15 @@
 ## Install Libraries ##
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(broom)) install.packages("broom", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(knitr)) install.packages("knitr", repos = "http://cran.us.r-project.org")
+if(!require(kableExtra)) install.packages("kableExtra", repos = "http://cran.us.r-project.org")
+if(!require(rpart)) install.packages("rpart", repos = "http://cran.us.r-project.org")
+if(!require(rattle)) install.packages("rattle", repos = "http://cran.us.r-project.org")
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
+if(!require(rpart.plot)) install.packages("rpart.plot", repos = "http://cran.us.r-project.org")
 
 #Load Data Files
 EndowFASB <- read.csv("https://raw.githubusercontent.com/drcdavidson/college-endowments/main/IPEDS_Data/EndowmentFASB.csv")
@@ -229,4 +238,7 @@ rm(INST)
 
 #Write Final Dataset to CSV
 write.csv(Colleges, "C:/Users/cdavi/OneDrive/Desktop/Documents/R_Projects/college-endowments/Colleges.csv", row.names=FALSE)
+
+#######################################################################
+
 
